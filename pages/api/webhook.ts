69e9568432f3372f4ff431c6efdc648c51602298
Andrei Palchys <andrei@palchys.com>
2022-11-24 12:44:01 +0200

    wip

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,8 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
+import * as crypto from "crypto";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const str =
+    typeof req.body === "string" ? req.body : JSON.stringify(req.body);
+  const hash = crypto
+    .createHmac("sha256", "1234567890")
+    .update(str)
+    .digest("base64");
+
   res.status(200).json({
+    match: hash === req.headers["x-api-signature-sha256"],
+    hash,
     body: req.body,
     headers: req.headers,
   });
